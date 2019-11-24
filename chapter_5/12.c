4db9b699550d03c4bc6258083fe20ef7ebc7116f
Renan Ivo <renanivom@gmail.com>
2019-11-24 20:29:45 -0300

    Split exercise 5/12 in shorter functions

--- chapter_5/12.c
+++ chapter_5/12.c
@@ -4,26 +4,15 @@
 #define MAX_LINE 1000
 #define MAX_INPUT_STR 1000
 
+void parse_arguments(char *argv[], int argc, int *start_at, int *tab_spaces_length);
 int str_to_int(char input[], int max_input_str);
-
 
 int main(int argc, char *argv[])
 {
-    int i, size, start_at = 0, tab_spaces_length = 4;
+    int size, start_at = 0, tab_spaces_length = 4;
     char line[MAX_LINE];
 
-    for (i = 0; i < argc; ++i) {
-        if (argv[1][0] == '-') {
-            switch (argv[i][1]) {
-                case 'm':
-                    start_at = str_to_int(argv[++i], MAX_INPUT_STR);
-                    break;
-                case 'n':
-                    tab_spaces_length = str_to_int(argv[++i], MAX_INPUT_STR);
-                    break;
-            }
-        }
-    }
+    parse_arguments(argv, argc, &start_at, &tab_spaces_length);
     printf("start at: %d\ntab spaces: %d\n", start_at, tab_spaces_length);
 
     while ((size = readline(line, MAX_LINE)) > 0) {
@@ -40,4 +29,21 @@ int str_to_int(char input[], int max_input_str) {
         number = 10 * number + (input[i] - '0');
     }
     return number;
+}
+
+void parse_arguments(char *argv[], int argc, int *start_at, int *tab_spaces_length) {
+    int i;
+
+    for (i = 0; i < argc; ++i) {
+        if (argv[1][0] == '-') {
+            switch (argv[i][1]) {
+                case 'm':
+                    *start_at = str_to_int(argv[++i], MAX_INPUT_STR);
+                    break;
+                case 'n':
+                    *tab_spaces_length = str_to_int(argv[++i], MAX_INPUT_STR);
+                    break;
+            }
+        }
+    }
 }
